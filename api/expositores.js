5f2c07b60b872c14631bd2cbb7af3644de60a9c0
Kauadp <kauadp1405@gmail.com>
2025-09-11 17:24:10 -0300

    Corrige APi serverless

---
+++ api/expositores.js
@@ -0,0 +1,17 @@
+// api/expositores.js
+import fetch from "node-fetch";
+
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      `https://api.airtable.com/v0/${process.env.BASE_ID}/${process.env.TABLE_NAME}`,
+      {
+        headers: { Authorization: `Bearer ${process.env.AIRTABLE_TOKEN}` }
+      }
+    );
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}

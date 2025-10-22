a26d8de84a9ba03a6ca1d5b2e3c8682f5f7f7818
Kauã Dias <kauadias@alfaiatariadeideias.com.br>
2025-10-22 18:38:43 -0300

    Segundo dia de auditoria EXG Outubro 2025

--- infograficos/data/correcao.R
+++ infograficos/data/correcao.R
@@ -5,10 +5,39 @@ library(tidyverse)
 dados <- read.csv('bling_export.csv', sep = ";")
 
 table(dados$Fornecedor)
+
+
 table(dados$Data.de.emissão)
 
 dados$Data.de.emissão <- str_extract(dados$Data.de.emissão, "[0-9]+/[0-9]+/[0-9]+")
 table(dados$Data.de.emissão)
 
 high <- dados |> 
-  filter(Fornecedor == "-")
+  filter(Fornecedor == "-" | Fornecedor == "HIGH COMPANY LTDA")
+
+dados |> 
+  filter(Nome.da.Loja == "Vans")
+
+table(high$Data.de.emissão)
+
+high |> 
+  group_by(Data.de.emissão) |> 
+  summarise(
+    (Valor.total)
+  )
+
+arezzo <- dados |> 
+  filter(Fornecedor == "ZZAB Comercio de Calcados Ltda. AREZZO")
+
+table(dados$Descrição)
+
+dados$Descrição
+
+str_detect(dados$Descrição, "CALCADOS")
+
+
+high |> 
+  filter(Número == 10811)
+
+write.csv(dados, "dados.csv", row.names = F)
+

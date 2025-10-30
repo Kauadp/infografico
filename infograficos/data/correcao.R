92156b5711feb2910f83278d4928187a8cccf0bf
Kauã Dias <kauadias@alfaiatariadeideias.com.br>
2025-10-30 17:46:05 -0300

    Estudo pós evento

--- infograficos/data/correcao.R
+++ infograficos/data/correcao.R
@@ -2,70 +2,87 @@ rm(list = ls())
 
 library(tidyverse)
 library(jsonlite)
+library(dunn.test)
+library(MASS)
 
 
 dados <- read.csv('bling_export.csv', sep = ";")
 
 table(dados$Fornecedor)
 
-
-table(dados$Data.de.emissão)
-
-dados$Data.de.emissão <- str_extract(dados$Data.de.emissão, "[0-9]+/[0-9]+/[0-9]+")
-table(dados$Data.de.emissão)
-
-high <- dados |> 
-<<<<<<< HEAD
-  filter(Fornecedor == "-" | Fornecedor == 'HIGH COMPANY LTDA')
-=======
-  filter(Fornecedor == "CIA. HERING.")
->>>>>>> e79378a2a4f2d738eb19b03c1b3a59f9307184ec
-
-dados |> 
-  filter(Nome.da.Loja == "Vans")
-
-table(high$Data.de.emissão)
-
-high |> 
-  group_by(Data.de.emissão) |> 
-  summarise(
-    (Valor.total)
-  )
-
-arezzo <- dados |> 
-  filter(Fornecedor == "ZZAB Comercio de Calcados Ltda. AREZZO")
-
-table(dados$Descrição)
-
-dados$Descrição
-
-str(arezzo)
-
-arezzo <- arezzo %>%
+dados <- dados |> 
   mutate(
-    # 1. Converte 'Quantidade' para numérico, usando o locale (separador decimal = vírgula)
-    Quantidade = parse_number(Quantidade, locale = locale(decimal_mark = ",")),
-    
-    # 2. Converte 'Preço.de.custo' para numérico, usando o locale
-    `Preço.de.custo` = parse_number(`Preço.de.custo`, locale = locale(decimal_mark = ",")),
-    
-    Valor.total = parse_number(`Valor.total`, locale = locale(decimal_mark = ","))
+    loja = case_when(
+      Fornecedor == "-" | Fornecedor == 'HIGH COMPANY LTDA' ~ "High",
+      Fornecedor == "VCI - VANGUARD CONFECCOES IMPORTADAS S/A" ~ "Aramis",
+      Fornecedor == "PASQUINI E PASQUINI CONFECCOES LTDA." ~ "Acostamento",
+      Fornecedor == "ZZAB Comercio de Calcados Ltda. AREZZO" | Fornecedor == "ZZAB Comercio de Calcados Ltda. VANS" ~ "ZZAB Comercio de Calcados Ltda."
+      )
   )
 
+table(dados$loja)
 
-arezzo$custo_total <- arezzo$Quantidade*arezzo$Preço.de.custo
+dados <- dados |>
+  mutate(
+    loja = case_when(
+      loja == "ZZAB Comercio de Calcados Ltda." & str_starts(Descrição, "CALCADOS") ~ "Arezzo",
+            loja == "ZZAB Comercio de Calcados Ltda." & !str_starts(Descrição, "CALCADOS") ~ "Vans",
+      
+      TRUE ~ loja
+    )
+  )
 
-sum(arezzo$Valor.total)
+table(dados$Data.de.emissão)
 
-sum(arezzo$custo_total, na.rm = T)
+dados <- dados |> 
+  mutate(
+    data = dmy_hms(Data.de.emissão),
+    data_emissao = as.Date(data),
+    hora_emissao = hour(data)
+  )
 
-arezzo_filtrado <- arezzo %>%
-  filter(str_starts(Descrição, "CALCADOS"))
+str(dados)
 
-sum(arezzo_filtrado$Valor.total)
+dados <- dados |> 
+  mutate(
+    Quantidade = parse_number(Quantidade, locale = locale(decimal_mark = ",")),
+    Preço.de.custo = parse_number(Preço.de.custo, locale = locale(decimal_mark = ",")),
+    Valor.total = parse_number(Valor.total, locale = locale(decimal_mark = ",")),
+    Valor.unitário = parse_number(Valor.unitário, locale = locale(decimal_mark = ",")),
+    Valor.total.líquido = parse_number(Valor.total.líquido, locale = locale(decimal_mark = ","))
+  ) 
 
-high |> 
-  filter(Número == 10811)
+dados <- dados |> 
+  mutate(
+    Preço.de.custo = case_when(
+      loja == "Arezzo" ~ 42,
+      TRUE ~ Preço.de.custo)
+  )
+
+dados <- dados |> 
+  mutate(
+    custo_total = Quantidade*Preço.de.custo
+  )
+
+peak_demanda <- as.data.frame.matrix(table(dados$data_emissao, dados$hora_emissao)) |>
+  rownames_to_column(var = "data_emissao") |>
+  pivot_longer(
+    cols = -data_emissao,
+    names_to = "hora_emissao",
+    values_to = "contagem"
+  ) |>
+  mutate(
+    hora_emissao = as.factor(hora_emissao),
+    data_emissao = ymd(data_emissao)
+  )
+
+peak_demanda <- peak_demanda |> 
+  mutate(
+  hora_emissao = as.factor(hora_emissao),
+  dia_da_semana = factor(weekdays(data_emissao, abbreviate = TRUE)) 
+)
+
+lm <- glm.nb(contagem ~ dia_da_semana + hora_emissao, data = peak_demanda)
 
 write.csv(dados, "dados.csv", row.names = F)
 
